a94f426b0054c708745291537dc8a79aba02abcb
pachara-camt <pachara.t@cmu.ac.th>
2020-10-13 21:48:25 +0700

    Add files via upload

---
+++ LazyPerson.php
@@ -0,0 +1,23 @@
+<?php
+require_once './ShowInfo.php';
+require_once './Person.php';
+
+class LazyPerson extends Person implements ShowInfo
+{
+    function runFor($km)
+    {
+        fprintf(STDERR, "I am a lazy man, I don't run.\n");
+        return false;
+    }
+    
+    function showInfo()
+    {
+        fprintf(STDERR, "I am too lazy to answer you.\n");
+        return false;
+    }
+    
+    function showLongInfo()
+    {
+        return $this->showInfo();
+    }
+}
---
+++ app.php
@@ -0,0 +1,52 @@
+<?php
+require_once './Car.php';
+require_once './SuperCar.php';
+require_once './Person.php';
+require_once './LazyPerson.php';
+
+function runAllFor($list, $km) {
+    foreach($list as $item) {
+        // to make sure $item has runFor method
+        if($item instanceof Runnable) {
+            $item->runFor($km);
+        }
+    }
+}
+
+function showAllInfo($list) {
+    foreach($list as $item) {
+        // if $item is an instance of ShowInfo
+        // call showLongInfo();
+        if($item instanceof ShowInfo) {
+            $item->showLongInfo();
+        }
+        // if $item is an instance of Runnable
+        // show runningDistance;
+        else if($item instanceof Runnable) {
+            printf("Distance: %d\n", $item->runningDistance());
+        }
+        // other case print unknow information
+        else {
+            printf("Unknown information.\n");
+        }
+        printf("%s\n", str_repeat('-', 40));
+    }
+}
+
+$car = new Car('Adam', 1800);
+$superCar = new SuperCar('Bob', 8000);
+$person = new Person('Cindy');
+$lazyPerson = new LazyPerson('Donald');
+
+$car->engineStart();
+$superCar->engineStart();
+
+$runningList = [$car, $superCar, $person, $lazyPerson];
+
+runAllFor($runningList, 20);
+printf("%s\n", str_repeat('=', 40));
+
+$car->engineStop();
+$superCar->engineStop();
+
+showAllInfo($runningList);
